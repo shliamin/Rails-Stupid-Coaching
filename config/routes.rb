529aa5ddbe22be3c4eec82e1b08dabc74a266cc6
Efim <efimshliamin@gmail.com>
2024-06-26 19:58:30 +0200

    postgresql added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#ask'
   get 'ask', to: 'pages#ask'
   get 'answer', to: 'pages#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
